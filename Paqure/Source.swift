cf233d3d64249a93c15f376800a1a7e4a0ad8017
Roderic Linguri <linguri@digices.com>
2016-07-19 23:52:16 -0600

    Create Source enum.

---
+++ Paqure/Source.swift
@@ -0,0 +1,17 @@
+//
+//  Source.swift
+//  Paqure
+//
+//  Created by Linguri Technology on 7/19/16.
+//  Copyright © 2016 Digices. All rights reserved.
+//
+
+import UIKit
+
+enum Source {
+    case NULL
+    case DEFAULT
+    case LOCAL
+    case REMOTE
+    case SYNCED
+}
